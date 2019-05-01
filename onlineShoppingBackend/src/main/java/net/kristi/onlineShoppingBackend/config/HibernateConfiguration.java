b1847cad80ace2d2b67c35cb8650ce903bc53412
pcn <pcn@192.168.0.177>
2019-05-01 13:53:51 +0200

    h2-configuration

--- onlineShoppingBackend/src/main/java/net/kristi/onlineShoppingBackend/config/HibernateConfiguration.java
+++ onlineShoppingBackend/src/main/java/net/kristi/onlineShoppingBackend/config/HibernateConfiguration.java
@@ -1,20 +1,64 @@
 package net.kristi.onlineShoppingBackend.config;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@ComponentScan(basePackages= {"net.kristi.onlineShoppingBackend.dto"})
+@ComponentScan(basePackages = { "net.kristi.onlineShoppingBackend.dto" })
 @EnableTransactionManagement
 public class HibernateConfiguration {
-	
+
 	private final static String DATABASE_URL = "jdbc:h2:tcp://localhost/~/onlineShopping";
 	private final static String DATABASE_DRIVER = "org.h2.Driver";
 	private final static String DATABASE_DIALECT = "org.hibernate.dialect.H2Dialect";
 	private final static String DATABASE_USERNAME = "root";
 	private final static String DATABASE_PASSWORD = "root";
 
-	
+	@Bean
+	private DataSource getDataSource() {
 
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(DATABASE_DRIVER);
+		dataSource.setUrl(DATABASE_URL);
+		dataSource.setUsername(DATABASE_USERNAME);
+		dataSource.setPassword(DATABASE_PASSWORD);
+		return dataSource;
+	}
+
+	// sessionFactory bean
+	@Bean
+	private SessionFactory getSessionFactory(DataSource dataSource) {
+
+		LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(dataSource);
+		builder.addProperties(getHibernateProperties());
+		builder.scanPackages("net.kristi.onlineShoppingBackend.dto");
+		return builder.buildSessionFactory();
+	}
+
+	// Return Hibernate Properties
+	private Properties getHibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", DATABASE_DIALECT);
+		properties.put("hibernate.show_sql", "true");
+		properties.put("hibernate.format_sql", "true");
+		return properties;
+
+	}
+
+	@Bean
+	private HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager(sessionFactory);
+		return hibernateTransactionManager;
+
+	}
 }
